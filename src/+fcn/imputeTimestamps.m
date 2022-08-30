4394299999c1e488bf2ce843a83db16f7df6cb2f
Joachim Vandekerckhove <joachim@uci.edu>
2022-08-30 01:23:18 -0700

    Refactor imputer

--- src/+fcn/imputeTimestamps.m
+++ src/+fcn/imputeTimestamps.m
@@ -1,40 +1,37 @@
 function raw_data = imputeTimestamps(raw_data)
 
-dummy_date = datetime('01-Jan-0000');      % empty datetimes are this sometimes
+dummy_date = datetime('01-Jan-0000'); % empty datetimes are this sometimes
 
-current    = datetime('28-Sep-0021 0:00'); % datetime tracker
+raw_data   = imputePartial(      ...
+    raw_data                   , ...
+    'student_survey_timestamp' , ...
+    '28-Sep-0021 0:00'           ...
+    );
 
-for r = 1:numel(raw_data.record_id) % loop over records
+raw_data   = imputePartial(                      ...
+    raw_data                                   , ...
+    'student_survey_new_participant_timestamp' , ...
+    '21-Jun-0022 0:00'                           ...
+    );
 
-    date_is_dummy   = raw_data.student_survey_timestamp(r) == dummy_date;
-    date_is_nat     = isnat(raw_data.student_survey_timestamp(r));
-    needs_replacing = date_is_dummy || date_is_nat;
-    
-    if needs_replacing
-        % if there is no date, assume previous date still holds
-        raw_data.student_survey_timestamp(r) = current;
-    else
-        % if there is a date, keep it and update tracker
-        current = raw_data.student_survey_timestamp(r);
+    function raw_data = imputePartial(raw_data, timestamp_field, current)
+
+        current = datetime(current);
+
+        for r = 1:numel(raw_data.record_id) % loop over records
+
+            date_is_dummy   = raw_data.(timestamp_field)(r) == dummy_date;
+            date_is_nat     = isnat(raw_data.(timestamp_field)(r));
+            needs_replacing = date_is_dummy || date_is_nat;
+
+            if needs_replacing
+                % if there is no date, assume previous date still holds
+                raw_data.(timestamp_field)(r) = current;
+            else
+                % if there is a date, keep it and update tracker
+                current = raw_data.(timestamp_field)(r);
+            end
+
+        end
     end
-
-end
-
-current    = datetime('21-Jun-0022 0:00');
-
-for r = 1:numel(raw_data.record_id) % loop over records
-
-    date_is_dummy   = raw_data.student_survey_new_participant_timestamp(r) == dummy_date;
-    date_is_nat     = isnat(raw_data.student_survey_new_participant_timestamp(r));
-    needs_replacing = date_is_dummy || date_is_nat;
-    
-    if needs_replacing
-        % if there is no date, assume previous date still holds
-        raw_data.student_survey_new_participant_timestamp(r) = current;
-    else
-        % if there is a date, keep it and update tracker
-        current = raw_data.student_survey_new_participant_timestamp(r);
-    end
-
-end
 end
